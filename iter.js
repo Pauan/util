7a3e93c4751349644258f3e438360352bde270f2
Pauan <pcxunlimited@gmail.com>
2013-07-08 23:41:03 -0700

    .

--- iter.js
+++ iter.js
@@ -1,68 +1,35 @@
 define(["./name", "./object"], function (name, object) {
   "use strict";
 
-  var iterator = new name.Name()
+  var hasOwn = {}.hasOwnProperty
 
-  // TODO: should be a unique [[Class]] but can't
-  function StopIteration() {}
-
-  function Iterator(x) {
-    // TODO is this spec-compliant?
-    if (!(this instanceof Iterator)) {
-      return new Iterator(x)
+  function toIterator(x) {
+    if (iterator in x) {
+      return x[iterator]()
+    } else if ("length" in x) {
+      var i = 0
+      return {
+        next: function () {
+          if (i < (x.length >>> 0)) {
+            return x[i++]
+          } else {
+            throw new StopIteration()
+          }
+        }
+      }
+    } else {
+      throw new TypeError(x + " does not have an @iterator or length property")
     }
   }
-  Iterator.prototype[iterator] = function () {
-    return this
-  }
 
-  // TODO not quite correct, should use {}.toString.call, but that returns "[object Object]"
-  function isStopIteration(x) {
-    return x instanceof StopIteration
-  }
-
-  // TODO: these return arrays, they should return iterators, but can't due to lack of generators
-  function allKeys(x) {
-    var r = []
-    for (var s in x) {
-      r.push(s)
+  function makeIterator(f) {
+    var o = {}
+    o[iterator] = function () {
+      return this
     }
-    return r
+    o.next = f
+    return o
   }
-
-  // TODO these can at least return iterators, even without generators
-  function allValues(x) {
-    return allKeys(x).map(function (s) {
-      return x[s]
-    })
-  }
-
-  function allItems(x) {
-    return allKeys(x).map(function (s) {
-      return [s, x[s]]
-    })
-  }
-
-  // TODO inefficient?
-  function keys(x) {
-    var has = {}.hasOwnProperty
-    return allKeys(x).filter(function (s) {
-      return has.call(x, s)
-    })
-  }
-
-  function values(x) {
-    return keys(x).map(function (s) {
-      return x[s]
-    })
-  }
-
-  function items(x) {
-    return keys(x).map(function (s) {
-      return [s, x[s]]
-    })
-  }
-
 
   // Non-standard but useful stuff
   function peekIterator(x) {
@@ -78,23 +45,14 @@ define(["./name", "./object"], function (name, object) {
     }
   }
 
+  // Strict evaluation
   function some(x, f) {
+    x = toIterator(x)
     try {
-      if (iterator in x) {
-        var o = x[iterator]()
-        while (true) {
-          if (f(o.next())) {
-            return true
-          }
+      while (true) {
+        if (f(x.next())) {
+          return true
         }
-      } else if ("length" in x) {
-        for (var i = 0; i < (x.length >>> 0); ++i) {
-          if (f(x[i])) {
-            return true
-          }
-        }
-      } else {
-        throw new TypeError(x + " does not have an @iterator or length property")
       }
     } catch (e) {
       if (!(e instanceof StopIteration)) {
@@ -127,16 +85,8 @@ define(["./name", "./object"], function (name, object) {
 
   function forin(x, f) {
     each(items(x), function (a) {
-      f(a[0], a[1])
+      return f(a[0], a[1])
     })
-  }
-
-  function map(x, f) {
-    var r = []
-    each(x, function (x) {
-      r.push(f(x))
-    })
-    return r
   }
 
   function has(x, y) {
@@ -145,14 +95,102 @@ define(["./name", "./object"], function (name, object) {
     })
   }
 
+  // Lazy evaluation
+  function map(x, f) {
+    x = toIterator(x)
+    return makeIterator(function () {
+      return f(x.next())
+    })
+  }
+
   function filter(x, f) {
-    var r = []
-    each(x, function (x) {
-      if (f(x)) {
-        r.push(x)
+    x = toIterator(x)
+    return makeIterator(function () {
+      while (true) {
+        var y = x.next()
+        if (f(y)) {
+          return y
+        }
       }
     })
+  }
+
+  function pair(x) {
+    x = toIterator(x)
+    return makeIterator(function () {
+      var l = x.next()
+        , r
+      try {
+        r = x.next()
+      } catch (e) {
+        if (!(e instanceof StopIteration)) {
+          throw e
+        }
+      }
+      return [l, r]
+    })
+  }
+
+
+  // Standard stuff
+  var iterator = new name.Name()
+
+  // TODO: should be a unique [[Class]] but can't
+  function StopIteration() {}
+
+  function Iterator(x) {
+    // TODO is this spec-compliant?
+    if (!(this instanceof Iterator)) {
+      return new Iterator(x)
+    }
+  }
+  Iterator.prototype[iterator] = function () {
+    return this
+  }
+
+  // TODO not quite correct, should use {}.toString.call, but that returns "[object Object]"
+  function isStopIteration(x) {
+    return x instanceof StopIteration
+  }
+
+  // TODO: this returns an array, it should return an iterator, but can't due to lack of generators
+  function allKeys(x) {
+    var r = []
+    for (var s in x) {
+      r.push(s)
+    }
     return r
+  }
+
+  function allValues(x) {
+    return map(allKeys(x), function (s) {
+      return x[s]
+    })
+  }
+
+  function allItems(x) {
+    return map(allKeys(x), function (s) {
+      return [s, x[s]]
+    })
+  }
+
+  // TODO inefficient?
+  function keys(x) {
+    return filter(allKeys(x), function (s) {
+      return hasOwn.call(x, s)
+    })
+  }
+
+  function values(x) {
+    return map(keys(x), function (s) {
+      return x[s]
+    })
+  }
+
+  function items(x) {
+    return map(keys(x), function (s) {
+      return [s, x[s]]
+    })
   }
 
   return {
@@ -171,13 +209,14 @@ define(["./name", "./object"], function (name, object) {
 
     // Non-standard
     peekIterator: peekIterator,
+    each: each, // TODO maybe rename to forOf or forof ?
     some: some,
     every: every,
-    each: each, // TODO maybe rename to forEach or forOf or forof ?
     length: length,
     forin: forin,
     map: map,
     has: has,
     filter: filter,
+    pair: pair,
   }
 })
