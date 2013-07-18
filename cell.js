31ba0855375ed540f392210062885a55b5880d93
Pauan <pcxunlimited@gmail.com>
2013-07-18 05:37:59 -0700

    .

--- cell.js
+++ cell.js
@@ -1,6 +1,11 @@
+// Extremely light-weight Functional Reactive Programming implementation
+// Everything is handled synchronously: no concurrency or parallelism
 define(["./object"], function (object) {
   "use strict";
 
+  // Reifies a JavaScript value as a signal
+  // use the get method to get the current value
+  // use the set method to set the current value
   function value(x) {
     var events = []
     return {
@@ -10,7 +15,7 @@ define(["./object"], function (object) {
       set: function (v) {
         x = v
         events.forEach(function (f) {
-          f()
+          f(x)
         })
       },
       // TODO binds duplicates if called multiple times; is this a problem?
@@ -29,33 +34,33 @@ define(["./object"], function (object) {
     }
   }
 
-  function call(a, f) {
-    return f.apply(null, a.map(function (x) {
-      return x.get()
-    }))
-  }
-
-  function on(a, f) {
-    var o = value(call(a, f))
-    function onset() {
-      o.set(call(a, f))
-    }
-    a.forEach(function (x) {
-      x.bind(onset)
+  // Takes an initial JavaScript value, a signal, and a function; returns a signal
+  // When the input signal changes, it will call the input function with the previous
+  // value and the current value of the input signal
+  function fold(init, x, f) {
+    var o = value(init)
+    x.bind(function (x) {
+      o.set((init = f(init, x)))
     })
     return o
   }
 
-  function fold(init, x, f) {
-    return on([x], function (x) {
-      return (init = f(init, x))
+  // Takes a signal and function; returns a signal
+  // Maps the function over the input signal
+  function map(x, f) {
+    return fold(f(x.get()), x, function (l, r) {
+      return f(r)
     })
   }
 
+  // Takes a signal; returns a signal
+  // Consecutive duplicates are ignored
+  // e.g. (1 -> 1 -> 2 -> 1 -> 1 -> 3 -> 3) is treated as
+  //      (1 -> 2 -> 1 -> 3)
   // TODO is there a better way to implement this?
   function dedupe(x) {
     var o = value(x.get())
-    on([x], function (x) {
+    map(x, function (x) {
       if (object.isnt(o.get(), x)) {
         o.set(x)
       }
@@ -65,8 +70,8 @@ define(["./object"], function (object) {
 
   return Object.freeze({
     value: value,
-    on: on,
     fold: fold,
+    map: map,
     dedupe: dedupe,
   })
 })
