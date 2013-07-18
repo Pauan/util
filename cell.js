4c3e7c81c505bb32af4bcf5456cbf81898d2d95c
Pauan <pcxunlimited@gmail.com>
2013-07-18 05:01:21 -0700

    .

--- cell.js
+++ cell.js
@@ -1,8 +1,33 @@
-define(["./name"], function (name) {
+define(["./object"], function (object) {
   "use strict";
 
-  var _value  = new name.Name()
-  var _events = new name.Name()
+  function value(x) {
+    var events = []
+    return {
+      get: function () {
+        return x
+      },
+      set: function (v) {
+        x = v
+        events.forEach(function (f) {
+          f()
+        })
+      },
+      // TODO binds duplicates if called multiple times; is this a problem?
+      bind: function (f) {
+        events.push(f)
+      },
+      unbind: function (f) {
+        var i = events.indexOf(f)
+        if (i !== -1) {
+          events.splice(i, 1)
+          if (events.length === 0) {
+
+          }
+        }
+      }
+    }
+  }
 
   function call(a, f) {
     return f.apply(null, a.map(function (x) {
@@ -10,59 +35,38 @@ define(["./name"], function (name) {
     }))
   }
 
-  function Const(x) {
-    this[_value]  = x
-    this[_events] = []
-  }
-  Const.prototype.get = function () {
-    return this[_value]
+  function on(a, f) {
+    var o = value(call(a, f))
+    function onset() {
+      o.set(call(a, f))
+    }
+    a.forEach(function (x) {
+      x.bind(onset)
+    })
+    return o
   }
 
-  function Value(x) {
-    Const.call(this, x)
-  }
-  Value.prototype = new Const()
-  Value.prototype.set = function (v) {
-    this[_value] = v
-    this[_events].forEach(function (f) {
-      f()
+  function fold(init, x, f) {
+    return on([x], function (x) {
+      return (init = f(init, x))
     })
   }
 
-  function On(a, f) {
-    Value.call(this)
-    var self = this
-    function onset() {
-      self.set(call(a, f))
-    }
-    // TODO binds duplicates if called multiple times
-    this.bind = function () {
-      onset()
-      a.forEach(function (x) {
-        x[_events].push(onset)
-      })
-    }
-    this.unbind = function () {
-      a.forEach(function (x) {
-        var i = x[_events].indexOf(onset)
-        if (i !== -1) {
-          x[_events].splice(i, 1)
-        }
-      })
-    }
-    this.bind()
+  // TODO is there a better way to implement this?
+  function dedupe(x) {
+    var o = value(x.get())
+    on([x], function (x) {
+      if (object.isnt(o.get(), x)) {
+        o.set(x)
+      }
+    })
+    return o
   }
-  On.prototype = new Value()
 
   return Object.freeze({
-    constant: function (x) {
-      return new Const(x)
-    },
-    value: function (x) {
-      return new Value(x)
-    },
-    on: function (a, f) {
-      return new On(a, f)
-    }
+    value: value,
+    on: on,
+    fold: fold,
+    dedupe: dedupe,
   })
 })
