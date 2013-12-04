a68de2d7feff78039586186356df87cea22f7c5b
Pauan <pcxunlimited@gmail.com>
2013-12-04 20:19:23 +0800

    .

--- time.js
+++ time.js
@@ -1,18 +1,19 @@
 define(function () {
   "use strict";
-  
+
   // TODO this probably isn't super-robust, but it should work for common cases
   var iMax = null
+
   function timestamp() {
     var x = Date.now()
-    if (iMax == null) {
+    if (iMax === null || x > iMax) {
       iMax = x
-    } else if (x <= iMax) {
+    } else {
       x = ++iMax
     }
     return x
   }
-  
+
   return Object.freeze({
     timestamp: timestamp,
   })
