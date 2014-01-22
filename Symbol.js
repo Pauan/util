0b1fca02ebca9bdf2ca7e57ee9064d8ba724e27e
Pauan <pcxunlimited@gmail.com>
2014-01-22 20:15:17 +0800

    .

--- Symbol.js
+++ Symbol.js
@@ -5,19 +5,16 @@ goog.scope(function () {
   var id  = 0
     , sId = "Symbol(0)"
 
-  function Symbol1(s) {
-    if (s == null) {
-      this[sId] = "Symbol(" + (++id) + ")"
-    } else {
-      this[sId] = "Symbol(\"" + s + "\", " + (++id) + ")"
-    }
-    Object.freeze(this) // TODO is this a good idea ?
-  }
-  Symbol1.prototype.toString = function () {
-    return this[sId]
-  }
-
   util.Symbol = function (s) {
-    return new Symbol1(s)
+    var o = {}
+    if (s == null) {
+      o[sId] = "Symbol(" + (++id) + ")"
+    } else {
+      o[sId] = "Symbol(\"" + s + "\", " + (++id) + ")"
+    }
+    o.toString = function () {
+      return o[sId]
+    }
+    return Object.freeze(o)
   }
 })
