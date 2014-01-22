9a1c097ddb3f1f3af271e14c99af7b2794fdde95
Pauan <pcxunlimited@gmail.com>
2014-01-23 00:13:11 +0800

    .

--- time.js
+++ time.js
@@ -1,50 +1,50 @@
-"use strict";
+goog.provide("util.time")
 
-// TODO this probably isn't super-robust, but it should work for common cases
-var iMax = null
+goog.scope(function () {
+  // TODO this probably isn't super-robust, but it should work for common cases
+  var iMax = null
 
-function timestamp() {
-  var x = Date.now()
-  if (iMax === null || x > iMax) {
-    iMax = x
-  } else {
-    x = ++iMax
-  }
-  return x
-}
-exports.timestamp = timestamp
-
-function benchmark(f, duration) {
-  if (duration == null) {
-    duration = 10000
+  util.time.timestamp = function () {
+    var x = goog.now()
+    if (iMax === null || x > iMax) {
+      iMax = x
+    } else {
+      x = ++iMax
+    }
+    return x
   }
 
-  var i   = 0
-    , t   = performance
-    , mem = {}
+  util.time.benchmark = function (f, duration) {
+    if (duration == null) {
+      duration = 10000
+    }
 
-  // TODO check this
-  for (var s in t.memory) {
-    mem[s] = t.memory[s]
+    var i   = 0
+      , t   = performance
+      , mem = {}
+
+    // TODO check this
+    for (var s in t.memory) {
+      mem[s] = t.memory[s]
+    }
+
+    var start = t.now()
+      , end   = start + duration
+      , curr
+
+    while ((curr = t.now()) < end) {
+      f()
+      ++i
+    }
+
+    for (var s in t.memory) {
+      mem[s] = t.memory[s] - mem[s]
+    }
+
+    return {
+      iterations: i,
+      milliseconds: (curr - start) / i,
+      memory: mem
+    }
   }
-
-  var start = t.now()
-    , end   = start + duration
-    , curr
-
-  while ((curr = t.now()) < end) {
-    f()
-    ++i
-  }
-
-  for (var s in t.memory) {
-    mem[s] = t.memory[s] - mem[s]
-  }
-
-  return {
-    iterations: i,
-    milliseconds: (curr - start) / i,
-    memory: mem
-  }
-}
-exports.benchmark = benchmark
+})
