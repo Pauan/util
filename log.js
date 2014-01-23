df90074599f8dcef47faec0b8518be87fcdcafa7
Pauan <pcxunlimited@gmail.com>
2014-01-24 07:47:22 +0800

    .

--- log.js
+++ log.js
@@ -1,9 +1,11 @@
 goog.provide("util.log")
 
 goog.scope(function () {
-  util.log.log = function (s) {
-    if (goog.DEBUG) {
-      console.log(s)
+  util.log.assert = function (x) {
+    if (goog.DEBUG && !x) {
+      throw new Error()
     }
   }
+
+  util.log.log = (goog.DEBUG ? goog.bind(console.log, console) : function () {})
 })
