00a7a4b167c14fd7f18ad9bdc4b9de9ed3bbbaab
Pauan <pcxunlimited@gmail.com>
2014-01-25 11:24:22 +0800

    .

--- log.js
+++ log.js
@@ -28,7 +28,7 @@ goog.scope(function () {
   }
 
   /**
-   * @param {...[?]} var_args
+   * @type {function(...[*]):void}
    */
-  util.log.log = (goog.DEBUG ? func.bind(console["log"], console) : function (var_args) {})
+  util.log.log = (goog.DEBUG ? func.bind(console["log"], console) : function () {})
 })
