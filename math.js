601398c92aa08c13b8c6d2d5c6b07414b6f27e09
Pauan <pcxunlimited@gmail.com>
2014-01-26 11:04:57 +0800

    .

--- math.js
+++ math.js
@@ -1,13 +1,62 @@
-"use strict";
+goog.provide("util.math")
 
-// http://www.johndcook.com/blog/2010/06/02/whats-so-hard-about-finding-a-hypotenuse/
-function hypot(x, y) {
-  x = Math.abs(x)
-  y = Math.abs(y)
-  // TODO could optimize a bit...
-  var max = Math.max(x, y)
-    , min = Math.min(x, y)
-    , r   = min / max
-  return max * Math.sqrt(1 + (r * r))
-}
-exports.hypot = hypot
+goog.scope(function () {
+  /**
+   * @param {number} x
+   * @return {number}
+   */
+  util.math.abs = function (x) {
+    if (x < 0) {
+      return -x
+    } else {
+      return x
+    }
+  }
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @return {number}
+   */
+  util.math.max = function (x, y) {
+    if (x < y) {
+      return y
+    } else {
+      return x
+    }
+  }
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @return {number}
+   */
+  util.math.min = function (x, y) {
+    if (x > y) {
+      return y
+    } else {
+      return x
+    }
+  }
+
+  /**
+   * @type {function(number):number}
+   */
+  util.math.sqrt = Math.sqrt
+
+  /**
+   * http://www.johndcook.com/blog/2010/06/02/whats-so-hard-about-finding-a-hypotenuse/
+   * @param {number} x
+   * @param {number} y
+   * @return {number}
+   */
+  util.math.hypot = function (x, y) {
+    x = util.math.abs(x)
+    y = util.math.abs(y)
+    // TODO could optimize a bit...
+    var max = util.math.max(x, y)
+      , min = util.math.min(x, y)
+      , r   = min / max
+    return max * util.math.sqrt(1 + (r * r))
+  }
+})
