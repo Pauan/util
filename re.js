b3dd29a5a1b6fdf14b56454fb5afde067b859b08
Pauan <pcxunlimited@gmail.com>
2014-01-31 12:21:50 +0800

    .

--- re.js
+++ re.js
@@ -4,18 +4,39 @@ goog.provide("util.re")
 goog.scope(function () {
   var top = /(?:)/
 
+  /**
+   * @param {string} str
+   * @param {!RegExp} re
+   * @return {boolean}
+   */
   util.re.test = function (str, re) {
-    top["test"]["call"](re, str)
+    return top["test"]["call"](re, str)
   }
 
+  /**
+   * @param {string} str
+   * @param {!RegExp} re
+   * @return {!Array.<string>}
+   */
   util.re.exec = function (str, re) {
-    top["exec"]["call"](re, str)
+    return top["exec"]["call"](re, str)
   }
 
+  /**
+   * @param {string} str
+   * @param {!RegExp} re
+   * @param {string|function(...string):string} rep
+   * @return {string}
+   */
   util.re.replace = function (str, re, rep) {
-    ""["replace"]["call"](str, re, rep)
+    return ""["replace"]["call"](str, re, rep)
   }
 
+  /**
+   * @param {string} str
+   * @param {!RegExp} re
+   * @return {!Array.<string>}
+   */
   util.re.split = function (str, re) {
     return ""["split"]["call"](str, re)
   }
